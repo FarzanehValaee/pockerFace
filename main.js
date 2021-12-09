8451be51b517addc27febdae8c5f1e9f76f76d05
Farzaneh <farzaneh.valaee79@gmail.com>
2021-12-09 14:38:24 +0330

    add commit

---
+++ main.js
@@ -0,0 +1,39 @@
+// const startPoint = {
+//   x: 100,
+//   y: 300,
+// };
+
+// const endPoint = {
+//   x: 300,
+//   y: 300,
+// };
+
+// drawLine(startPoint, endPoint);
+
+const centerPoint = {
+  x: 400,
+  y: 200,
+};
+
+
+drawCircle(centerPoint, 100);
+const Righteye={
+  x:450,
+  y:175
+}
+const Lefteye={
+  x:350,
+  y:175
+}
+drawCircle(Righteye,15)
+drawCircle(Lefteye,15)
+const startPointPocker = {
+  x: 350,
+  y: 250,
+};
+
+const endPointPocker = {
+  x: 450,
+  y: 250,
+};
+drawLine(startPointPocker,endPointPocker)
